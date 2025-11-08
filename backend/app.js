e22582ab9c41d39229bc6a4426aecd0409820e13
nirajj9393 <nirajreddynnr@gmail.com>
2025-11-08 15:11:37 +0530

    set up socket.io database etc..

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {createServer} from "node:http";
+
+
+import mongoose from "mongoose";
+import cors from 'cors';
+import dotenv from 'dotenv';   
+
+import connectToSocket from "./src/controller/socketManager.js";
+
+dotenv.config(); 
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 3000));
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.get("/", (req, res) => {
+  res.send("Hello Niraj Bhai!");
+});
+
+const start = async () =>{
+    const connectToDb = await mongoose.connect(process.env.DB_URL);
+    console.log("connected to db....")
+    server.listen(app.get("port"),()=>{
+    console.log(`server is running on the port: ${process.env.PORT}`);
+
+    })
+}
+start();
